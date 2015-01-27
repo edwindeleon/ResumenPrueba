c5e478271a43a9ac4d30a4a1862733cf03e770e7
Edwin <mardwintrader@gmail.com>
2015-01-27 18:33:18 -0400

    commit 30

--- www/js/index.js
+++ www/js/index.js
@@ -45,5 +45,66 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        
+        var pushNotification = window.plugins.pushNotification; 
+        if (device.platform == 'android' || device.platform == 'Android') { 
+            alert("Register called"); 
+            //tu Project ID aca!! 
+            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"PROJECT_ID","ecb":"app.onNotificationGCM"}); 
+        } 
+        else { 
+            alert("Register called"); 
+            pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"}); 
+        } 
+    }, 
+    // result contains any message sent from the plugin call 
+    successHandler: function(result) { 
+        alert('Callback Success! Result = '+result) 
+    }, 
+    errorHandler:function(error) { 
+        alert(error); 
+    }, 
+    onNotificationGCM: function(e) { 
+        switch( e.event ) 
+        { 
+            case 'registered': 
+                if ( e.regid.length > 0 ) 
+                { 
+                    console.log("Regid " + e.regid); 
+                    alert('registration id = '+e.regid); 
+                    //Cuando se registre le pasamos el regid al input 
+                    document.getElementById('regId').value = e.regid; 
+                } 
+            break; 
+
+            case 'message': 
+              // NOTIFICACION!!! 
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt); 
+            break; 
+
+            case 'error': 
+              alert('GCM error = '+e.msg); 
+            break; 
+
+            default: 
+              alert('An unknown GCM event has occurred'); 
+              break; 
+        } 
+    }, 
+    onNotificationAPN: function(event) { 
+        var pushNotification = window.plugins.pushNotification; 
+        alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert); 
+         
+        if (event.alert) { 
+            navigator.notification.alert(event.alert); 
+        } 
+        if (event.badge) { 
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge); 
+        } 
+        if (event.sound) { 
+            var snd = new Media(event.sound); 
+            snd.play(); 
+        } 
+    
     }
 };
